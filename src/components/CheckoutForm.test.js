ce5ee186f8bc5dc5be882a32a587fc6b24635e3a
ferengitooth <ehitsamill@gmail.com>
2021-07-09 11:16:20 -0700

    imported userEvent into CheckoutForm.test. Both tests now passing

--- src/components/CheckoutForm.test.js
+++ src/components/CheckoutForm.test.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
 import CheckoutForm from "./CheckoutForm";
 
 // Write up the two tests here and make sure they are testing what the title shows
@@ -13,7 +14,7 @@ test("form header renders", async () => {
 });
 
 test("form shows success message on submit with form details", async () => {
-    render(<CheckoutForm/>)
+    render (<CheckoutForm />)
 
     const firstName = screen.getByLabelText("First Name:")
     const lastName = screen.getByLabelText("Last Name:")
